6953d02899d475fd02185e5b82852492fbf1770a
duranginethmini <durangineth02@gmail.com>
2025-03-12 22:33:23 +0530

    "" get all Income completed"

--- controllers/incomeController.js
+++ controllers/incomeController.js
@@ -27,7 +27,16 @@ exports.addIncome = async (req, res) => {
 }
 
 // getAll income source
-exports.getAllIncome = async (req, res) => {}
+exports.getAllIncome = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const income = await Income.find({userId}).sort({date: -1});
+        res.json(income);
+    }catch (err){
+        res.status(500).json({message:"Internal server error"})
+    }
+}
 
 // delete income source
 exports.deleteIncome = async (req, res) => {}
