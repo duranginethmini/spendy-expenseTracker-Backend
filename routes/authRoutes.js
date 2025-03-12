628542bcbbe01dfb06000e6a7a632cab835982eb
duranginethmini <durangineth02@gmail.com>
2025-03-12 17:17:42 +0530

    "Upload images completed"

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,10 +7,24 @@ const {
     getUserInfo,
 } = require("../controllers/authController");
 
+const upload = require("../middleware/uploadMiddleware");
+
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/getUser", protect, getUserInfo);
+
+// Image upload route
+router.post('/upload-image', upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+    }
+
+    // Correct URL construction
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+
+    res.status(200).json({ imageUrl });
+});
 
 module.exports = router;
